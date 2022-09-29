fe60ce7ed9d1864df2376d36de8330871dcb2c30
valuvilla <113980834+valuvilla@users.noreply.github.com>
2022-09-29 11:10:39 +0000

    lasaña

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-def factorial(n):
-  if n == 0:
-    return 1
-  else:
-    return n * factorial(n - 1)
-print(factorial(5))
+
+# TODO: define the 'EXPECTED_BAKE_TIME' constant
+EXPECTED_BAKE_TIME = 40
+# TODO: consider defining the 'PREPARATION_TIME' constant
+#       equal to the time it takes to prepare a single layer
+PREPARATION_TIME = 2
+# TODO: define the 'bake_time_remaining()' function
+def bake_time_remaining(elapsed_bake_time):
+    """Calculate the bake time remaining.
+    
+    :param elapsed_bake_time: int - baking time already elapsed.
+    :return: int - remaining bake time (in minutes) derived from 'EXPECTED_BAKE_TIME'.
+ 
+
+    Function that takes the actual minutes the lasagna has been in the oven as
+    an argument and returns how many minutes the lasagna still needs to bake
+    based on the `EXPECTED_BAKE_TIME`.
+    """
+    return EXPECTED_BAKE_TIME - elapsed_bake_time
+    
+# TODO: define the 'preparation_time_in_minutes()' function
+#       and consider using 'PREPARATION_TIME' here
+def preparation_time_in_minutes(number_of_layers):
+    """return preparation time"""
+    return number_of_layers * (PREPARATION_TIME)
+# TODO: define the 'elapsed_time_in_minutes()' function
+def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
+    """return elapsed time"""
+    return preparation_time_in_minutes(number_of_layers) + elapsed_bake_time
